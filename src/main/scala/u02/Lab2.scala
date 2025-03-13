f812759fa9f1ce321af5ee2b11d8048abf6eb8c7
Denise Nanni <denise.nanni1@gmail.com>
2025-03-13 19:02:18 +0100

    test: multiplication evaluation

--- src/main/scala/u02/Lab2.scala
+++ src/main/scala/u02/Lab2.scala
@@ -18,12 +18,18 @@ object Lab2 extends App:
       assertEquals(5, evaluate(e))
     }
 
+    @Test def testMultiplyEvaluationWithLiterals(): Unit = {
+      val e = Expr.Multiply(Expr.Literal(3), Expr.Literal(2))
+      assertEquals(6, evaluate(e))
+    }
+
   enum Expr:
     case Literal(n: Int)
     case Add(e1: Expr, e2: Expr)
-    case Multiply(e1: Expr, e1: Expr)
+    case Multiply(e1: Expr, e2: Expr)
 
   object Expr:
     def evaluate(e: Expr): Int = e match
       case Expr.Literal(n) => n
       case Expr.Add(e1, e2) => evaluate(e1) + evaluate(e2)
+      case Expr.Multiply(e1, e2) => evaluate(e1) * evaluate(e2)
