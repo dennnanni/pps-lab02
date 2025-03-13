1abd2c7a16ead01ab68343605b6e3ea9fcd51e3c
Denise Nanni <denise.nanni1@gmail.com>
2025-03-13 18:56:45 +0100

    test: evaluation of a literal

--- src/main/scala/u02/Lab2.scala
+++ src/main/scala/u02/Lab2.scala
@@ -1,5 +1,23 @@
 package u02
 
-gimport org.junit.*
+import org.junit.*
+import org.junit.Assert.*
+import u02.Lab2.Expr.*
 
+object Lab2 extends App:
 
+  // Task 4
+  class Task4Test:
+    @Test def testExpressionCreation(): Unit = {
+      val e = Expr.Literal(5)
+      assertEquals(5, evaluate(e))
+    }
+
+  enum Expr:
+    case Literal(n: Int)
+    case Add(n1: Expr, n2: Expr)
+    case Multiply(n1: Expr, n2: Expr)
+
+  object Expr:
+    def evaluate(e: Expr): Int = e match
+      case Expr.Literal(n) => n
